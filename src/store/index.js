40d786a338d4b143ff537f591bb0d9e6bb50bdc9
hamadani9991 <hamadanibaroni@gmail.com>
2021-07-16 18:14:45 +0300

    part-2

--- src/store/index.js
+++ src/store/index.js
@@ -1,18 +1,26 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import EventService from '../services/EventService'
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
     user: {id: 'name123', name: 'Adam'},
-    categories: ['welfare', 'animal']
+    categories: ['welfare', 'animal'],
+    events: []
   },
   mutations: {
-
+    ADD_EVENT(state, event) {
+      state.events.push(event)
+    }
   },
   actions: {
-
+    createEvent({commit}, event) {
+    return EventService.postEvent(event).then(() => {
+      commit('ADD_EVENT', event)
+    })
+    }
   },
   modules: {
 
